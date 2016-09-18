7adc7b32af348eadc74eb5aed074cf96da01b208
Luis H. Matos <ReachLuis@Gmail.com>
2016-09-18 15:41:28 -0500

    0.2.5 - Make sure task is not an empty string

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -3,28 +3,47 @@ console.log('scripts.js sourced!');
 var verbose = true; // if (verbose) {console.log('');}
 
 /// == Function Declarations == ///
+function addTask(){
+  // empty Message box
+  clearMessage();
+  // grab new task name
+  var newTaskName = $('#newTaskText').val();
+  // check for empty string
+  if (newTaskName===''){
+    // if empty then disply message to user
+    $('#messageBox').html('<p>Cannot create blank task. Please enter a task below.</p>');
+  } else { // otherwise proceed with creating a task
+    // prepare the task to be sent
+    var objectToSend={
+      taskName: newTaskName
+    };
+    // ajax post code that sends object to /addTask route
+    $.ajax({
+      type: 'POST',
+      url: '/addTask',
+      data: objectToSend,
+      success: function( data ){
+        if (verbose) {console.log( 'got this from /addTask - ' + data );}
+        // Notify user of success
+        $('#messageBox').html('<p>Task created successfully!</p>');
+      },
+      statusCode: {
+        404: function(){
+          $('#messageBox').html('<p>404 Error! Please contact Support.</p>');
+        }
+      }
+    }); // end Ajax post code
+  }
+}
 
+function clearMessage(){
+  $('#messageBox').html('');
+}
 
 /// == JavaScript == ///
 
 $(document).ready(function(){
   if (verbose) {console.log('Document ready!');}
-
-  // // Ajax code for a API hit
-  // var genericUrl = 'http://www.'+'';
-  // $.ajax({
-  //   url: genericUrl,
-  //   dataType: 'JSON',
-  //   success: function(data){
-  //     if (verbose) {console.log('in Ajax success, with data:', data);}
-  //
-  //   },
-  //   statusCode: {
-  //     404: function(){
-  //       alert('404 Error! Cannot load page');
-  //     }
-  //   }
-  // }); // end ajax API code
 
   // // ajax get call to server to get object
   //   $.ajax({
@@ -35,33 +54,8 @@ $(document).ready(function(){
   //     } // end success
   //   }); //end ajax
 
-$('#addTask').on('click', function(){
-
-  var newTaskName = $('#newTaskText').val();
-
-  if (newTaskName===''){
-    
-  }
-
-  var objectToSend={
-    taskName:
-  };
-
-  // ajax post code that sends object to /routename route
-  $.ajax({
-    type: 'POST',
-    url: '/addTask',
-    data: objectToSend,
-    success: function( data ){
-      if (verbose) {console.log( 'got this from /addTask - ' + data );}
-    },
-    statusCode: {
-      404: function(){
-        alert('404 Error! Cannot load page');
-      }
-    }
-  }); // end Ajax post code
-
-}); // end #addTask.on('click')
+  $('#addTask').on('click', function(){
+    addTask();
+  }); // end #addTask.on('click')
 
 }); // end document ready
